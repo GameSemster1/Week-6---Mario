1cdc55f26c430eca45d62816cf6736d6b2d11b69
emmanuel405 <57897803+emmanuel405@users.noreply.github.com>
2020-11-27 13:02:34 +0200

    player & enemy

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+    
+    [SerializeField] float speed = 7f;
+    [SerializeField] Transform start = null;
+    [SerializeField] Transform end = null; 
+
+    private Vector3 velocity;
+    bool moveFromStartToEnd = true;
+    void FixedUpdate()
+    {
+        float deltaX = speed * Time.fixedDeltaTime;
+        if (moveFromStartToEnd) {
+            transform.position = Vector3.MoveTowards(transform.position, end.position, deltaX);
+        } else {  // move from end to start
+            transform.position = Vector3.MoveTowards(transform.position, start.position, deltaX);
+        }
+        
+        if (transform.position == start.position) {
+            moveFromStartToEnd = true;
+        } else if (transform.position == end.position) {
+            moveFromStartToEnd = false;
+        }
+        
+    }
+
+}
